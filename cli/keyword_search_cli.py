58c16f0a1213c5f59e9c663b3bd017e121a6268f
simddev <175357996+simddev@users.noreply.github.com>
2026-05-10 18:34:38 +0200

    split

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,4 +1,21 @@
+import string
 import argparse
+import json
+
+translator = str.maketrans("", "", string.punctuation)
+
+
+def tokenize(text: str) -> list[str]:
+    return [token for token in text.lower().translate(translator).split() if token]
+
+
+def matches(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for q in query_tokens:
+        for t in title_tokens:
+            if q in t:
+                return True
+    return False
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -11,10 +28,28 @@ def main() -> None:
 
     match args.command:
         case "search":
-            # print the search query here
-            print(f'Searching for: {args.query}')
+            query_tokens = tokenize(args.query)
+
+            with open("data/movies.json", "r", encoding="utf-8") as f:
+                data = json.load(f)
+
+            results = []
+
+            for movie in data["movies"]:
+                title_tokens = tokenize(movie["title"])
+
+                if matches(query_tokens, title_tokens):
+                    results.append(movie)
+
+            results = results[:5]
+
+            print(f"Searching for: {args.query}")
+            for i, movie in enumerate(results, start=1):
+                print(f"{i}. {movie['title']}")
+
         case _:
             parser.print_help()
+
 
 if __name__ == "__main__":
     main()
